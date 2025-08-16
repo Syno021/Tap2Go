1bd99f981aa4488b64f65d5d3a81d2fd94e7b9ff
Jabulani00 <jabulanimosesgwala@gmail.com>
2025-08-16 04:14:38 +0200

    t

--- index.web.js
+++ index.web.js
@@ -1,7 +1,7 @@
-import './src/polyfills';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import './src/polyfills';
 
 // Register the app
 AppRegistry.registerComponent(appName, () => App);
